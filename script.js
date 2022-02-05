817617328e1eed604343126f0853284b32c6d2ec
Logan Parke <logan.c.parke@gmail.com>
2022-02-04 16:00:23 -0800

    fixed totals

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ var tip = 1;
 function f1() {
     var bill = parseInt(document.getElementById("bill").value);
     var people = parseInt(document.getElementById("people").value);
-    tip = tipFunc();
+    tip = .15;
     
     var billTip = bill * tip;
     var finalAmt = billTip + bill;
     console.log(bill, tip, billTip, finalAmt);
-    var perPerson = finalAmt / people;
+    var perPersonTotal = finalAmt / people;
+    var perPersonTip = billTip / people;
 
+    document.getElementById("total-tip").innerHTML = "$ " + billTip.toFixed(2);
     document.getElementById("total-amt").innerHTML = "$ " + finalAmt.toFixed(2);
-    document.getElementById("per-person").innerHTML = "$ " + perPerson.toFixed(2);
+    document.getElementById("per-person").innerHTML = "$ " + perPersonTotal.toFixed(2);
+    document.getElementById("per-person-tip").innerHTML = "$ " + perPersonTip.toFixed(2);
 }
 
-function tipFunc() {
 
-}
